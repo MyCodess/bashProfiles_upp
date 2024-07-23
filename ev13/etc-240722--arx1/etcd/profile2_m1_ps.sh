70ca4e54165538f1ab5ec69967607b31411c5102
u1 <mypoosst-1@yahoo.com>
2024-07-23 10:29:25 +0200

    synced with ofc1 and adapted on arx1

--- ev13/etc-240722--arx1/etcd/profile2_m1_ps.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-q_pls1  "${BASH_SOURCE[0]##*/}"         ##-- psl ".. ${q_Profile2FN}"
-##--II- just a dummy-template  !!
-q_ple1  "${BASH_SOURCE[0]##*/}"         ##-- pel ".. ${q_Profile2FN}"
-
--- ev13/etc-240722--arx1/etcd/profile_2310msw_pos.sh
+++
@@ -1,9 +0,0 @@
-##__  #!/bin/bash
-set -a
-q_pls1  "${BASH_SOURCE[0]##*/}"         ##--prev:  export myname11="${BASH_SOURCE[0]##*/}" ; psl  "$myname11" ;
-
-#--??  q_syysTgL1=${q_hostTg}
-#--??  q_syysTgL2=${q_syysTgL1}_${q_osRelTg}
-#####  ==========  ofc1pc1-addies/settings:
-
-q_ple1  "${BASH_SOURCE[0]##*/}"
