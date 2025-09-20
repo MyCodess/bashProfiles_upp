fc972d1f79b66335bd8be5c747490d40dd17cf09
u1-arx1 <mypoosst-1@yahoo.com>
2025-09-20 09:04:10 +0200

    br1-commit--for-pr

---
+++ ev13/bin/bups1homesrest.sh
@@ -0,0 +1,18 @@
+##--I- bup of etcu-rest, which is not in etcu, but still under home!
+set -u
+set -C  #noclobber
+
+date11=$($cuds)
+targetDP="${ProgsResW1DP}/${date11}-${USER}-homesy"  ;
+[[ ! -d ${targetDP} ]] && ( mkdir -v  ${targetDP} || exit 3 )  ; ##--I- extra keine -p ! its root must already exist, otherwise exit!!
+cd ${targetDP} || exit 3
+
+cp  -iv  --parents  --preserve=timestamp  ${filesBM1_FP}  ${IntBM1_FP}  ${IntBM2_FP}  ./
+cp  -iv  --parents  --preserve=timestamp  ~/.config/vivaldi/Default/Preferences   ./
+cp  -iv  --parents  --preserve=timestamp  ~/.config/chromium/Default/Preferences   ./
+
+##--OK-if-no-VARs-using:   =================================
+#cp  -iv  --parents  --preserve=timestamp  ~/.config/vivaldi/Default/Bookmarks  ~/.config/vivaldi/Default/Preferences     ./
+#cp  -iv  --parents  --preserve=timestamp  ~/.config/chromium/Default/Bookmarks  ~/.config/chromium/Default/Preferences   ./
+
+
